4baf33888828f5f534b027c66c2107d8014f7824
710cabs <vitorscabrall@gmail.com>
2023-08-31 21:33:18 -0400

    Correção Diagrama

--- ProjetoEscola/src/Tecnico.java
+++ ProjetoEscola/src/Tecnico.java
@@ -2,29 +2,22 @@ import java.time.LocalDate;
 
 public class Tecnico extends BaseFuncionario {
     
-    private String cracha;
     private String setor;
-   
-    public String getCracha() {
-        return cracha;
-    }
-    public void setCracha(String cracha) {
-        this.cracha = cracha;
-    }
+
     public String getSetor() {
         return setor;
     }
+
     public void setSetor(String setor) {
         this.setor = setor;
     }
-    
+
     public Tecnico(int codigo, LocalDate data, String endereco, String telefone, String nome, String rg, String cpf,
             LocalDate data2, String cracha, String registro, LocalDate dataVinculoInicial, LocalDate dataVinculoFinal,
-            String cracha2, String setor) {
+            String setor) {
         super(codigo, data, endereco, telefone, nome, rg, cpf, data2, cracha, registro, dataVinculoInicial,
                 dataVinculoFinal);
-        cracha = cracha2;
         this.setor = setor;
     }
-
+   
 }
